5df6d166fcece9a7aea4b42c58d470f3e83dfbe3
ArtyomSprdnv <92688941+ArtyomSprdnv@users.noreply.github.com>
2022-05-16 09:41:19 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$user = 'u41026';
-$pass = '4433573';
+$user = 'u47502';
+$pass = '8701243';
 $db = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
