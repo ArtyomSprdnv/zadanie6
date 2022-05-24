c8dbb9b9534c54eb31837bc011ab9871f41edbdd
ggggggdb <92688941+ggggggdb@users.noreply.github.com>
2022-05-25 02:13:40 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 $user = 'u47502';
 $pass = '8701243';
-$db = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$db = new PDO('mysql:host=localhost;dbname=u47502', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
